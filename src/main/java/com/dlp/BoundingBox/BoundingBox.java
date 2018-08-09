ac16a304b4e802a4066147088183808351aafb86
Fabian Nagel <fabian.nagel@live.de>
2018-08-09 20:44:31 +0200

    Warning for dry run added

--- src/main/java/com/dlp/BoundingBox/BoundingBox.java
+++ src/main/java/com/dlp/BoundingBox/BoundingBox.java
@@ -69,7 +69,7 @@ public class BoundingBox {
     }
 
     public boolean isCorrupted() {
-        return coordinatesCorrupted() || dimensionsCorrupted();
+        return coordinatesCorrupted() || dimensionsCorrupted() || dimensionsToSmall();
     }
 
     // if x or y are bigger than they should be
@@ -86,6 +86,11 @@ public class BoundingBox {
                 this.getBounding_box_height() == 0 ||
                 this.getBounding_box_width() > DataSetConstants.IMAGE_WIDTH ||
                 this.getBounding_box_height() > DataSetConstants.IMAGE_HEIGHT;
+    }
+
+    private boolean dimensionsToSmall() {
+        return this.getBounding_box_width() < 10 ||
+                this.getBounding_box_height() < 10;
     }
 
     @Override
